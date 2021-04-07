a3e4b6d26c5fb0780780a018c57028755869a36d
Harsh Shandilya <me@msfjarvis.dev>
2021-04-07 17:32:07 +0530

    lib: apply ktfmt-gradle plugin and reformat
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,26 +1,32 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.4.32"
-    id("org.jetbrains.dokka") version "1.4.30"
-    id("com.vanniktech.maven.publish") version "0.13.0"
+  id("org.jetbrains.kotlin.jvm") version "1.4.32"
+  id("org.jetbrains.dokka") version "1.4.30"
+  id("com.vanniktech.maven.publish") version "0.13.0"
+  id("com.ncorti.ktfmt.gradle") version "0.5.0"
 }
 
 repositories {
-    mavenCentral()
-    maven("https://dl.bintray.com/kotlin/kotlinx") {
-      name = "KotlinX Bintray"
-      content {
-        includeModule("org.jetbrains.kotlinx", "kotlinx-html-jvm")
-      }
+  mavenCentral()
+  maven("https://dl.bintray.com/kotlin/kotlinx") {
+    name = "KotlinX Bintray"
+    content {
+      includeModule("org.jetbrains.kotlinx", "kotlinx-html-jvm")
     }
+  }
 }
 
 dependencies {
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+  implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+  testImplementation("org.jetbrains.kotlin:kotlin-test")
+  testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
 
 signing {
   useGpgCmd()
+}
+
+ktfmt {
+  googleStyle()
+  maxWidth.set(100)
 }
--- lib/src/test/kotlin/dev/sphericalkat/sublimefuzzy/FuzzyTest.kt
+++ lib/src/test/kotlin/dev/sphericalkat/sublimefuzzy/FuzzyTest.kt
@@ -6,29 +6,29 @@ import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 class FuzzyTest {
-    @Test
-    fun `fuzzy match simple success`() {
-        val result = Fuzzy.fuzzyMatchSimple("test", "tests")
-        assertTrue(result, "Each character in pattern should be found sequentially in str")
-    }
+  @Test
+  fun `fuzzy match simple success`() {
+    val result = Fuzzy.fuzzyMatchSimple("test", "tests")
+    assertTrue(result, "Each character in pattern should be found sequentially in str")
+  }
 
-    @Test
-    fun `fuzzy match simple failure`() {
-        val result = Fuzzy.fuzzyMatchSimple("test", "tset")
-        assertFalse(result, "Each character in pattern should be found sequentially in str")
-    }
+  @Test
+  fun `fuzzy match simple failure`() {
+    val result = Fuzzy.fuzzyMatchSimple("test", "tset")
+    assertFalse(result, "Each character in pattern should be found sequentially in str")
+  }
 
-    @Test
-    fun `fuzzy match success`() {
-        val (match, score) = Fuzzy.fuzzyMatch("fot", "FbxImporter.h")
-        assertTrue(match)
-        assertEquals(105, score)
-    }
+  @Test
+  fun `fuzzy match success`() {
+    val (match, score) = Fuzzy.fuzzyMatch("fot", "FbxImporter.h")
+    assertTrue(match)
+    assertEquals(105, score)
+  }
 
-    @Test
-    fun `fuzzy match failure`() {
-        val (match, score) = Fuzzy.fuzzyMatch("fot", "kot")
-        assertFalse(match)
-        assertEquals(0, score)
-    }
+  @Test
+  fun `fuzzy match failure`() {
+    val (match, score) = Fuzzy.fuzzyMatch("fot", "kot")
+    assertFalse(match)
+    assertEquals(0, score)
+  }
 }
