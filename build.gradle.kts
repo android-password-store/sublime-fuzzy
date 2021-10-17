3620a2d8949d7c6f9bcd563b9c46ba564f8923c7
Harsh Shandilya <me@msfjarvis.dev>
2021-10-17 17:53:59 +0530

    build: remove subproject and flatten project structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,44 @@
-buildscript {
-  repositories {
-    mavenCentral()
-  }
+plugins {
+  kotlin("multiplatform")
+  id("org.jetbrains.dokka")
+  id("com.ncorti.ktfmt.gradle")
+  id("org.jetbrains.kotlinx.binary-compatibility-validator")
+  id("convention.publication")
+}
 
-  dependencies {
-    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+configure<org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension> {
+  explicitApi()
+  jvm()
+  js(BOTH) {
+    browser()
+    nodejs()
+    compilations.all {
+      kotlinOptions {
+        moduleKind = "umd"
+        sourceMap = true
+        sourceMapEmbedSources = null
+      }
+    }
   }
+  ios()
+  linuxX64()
+  mingwX64()
+  macosX64()
+}
+
+kotlin {
+  sourceSets {
+    val commonMain by getting
+    val commonTest by getting {
+      dependencies {
+        implementation("org.jetbrains.kotlin:kotlin-test")
+        implementation("org.jetbrains.kotlin:kotlin-test-junit")
+      }
+    }
+  }
+}
+
+ktfmt {
+  googleStyle()
+  maxWidth.set(100)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,22 @@
 rootProject.name = "sublime-fuzzy"
-include("lib")
-includeBuild("convention-plugins")
+
+pluginManagement {
+  repositories {
+    gradlePluginPortal()
+    mavenCentral()
+    includeBuild("convention-plugins")
+  }
+  plugins {
+    kotlin("multiplatform") version "1.5.31" apply false
+    id("org.jetbrains.dokka") version "1.5.31" apply false
+    id("com.ncorti.ktfmt.gradle") version "0.7.0" apply false
+    id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.8.0-RC" apply false
+    id("convention.publication") apply false
+  }
+}
+
+dependencyResolutionManagement {
+  repositories {
+    mavenCentral()
+  }
+}
